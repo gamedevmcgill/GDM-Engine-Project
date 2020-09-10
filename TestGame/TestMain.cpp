be1902185f5ac53d88df0a302ecb88f6d3452450
Roey Borsteinas <roeyb1@hotmail.com>
2020-09-09 21:01:19 -0400

    Implemented a basic logging macro

--- TestGame/TestMain.cpp
+++ TestGame/TestMain.cpp
@@ -1,7 +1,11 @@
 #include "Renderer/Renderer.h"
+#include "Core/Core.h"
 
 int main()
 {
 	GDMEngine::TestLibraries();
+
+	GDMEngine::Logger::Get().SetLogFile("Log.txt");
+	GDM_LOG(LogInfo, "Hello world from GameDev McGill");
 	return 0;
 }
